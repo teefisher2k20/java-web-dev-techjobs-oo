43a392f65e0496f3d22adfae81bda46e528e7885
teefisher2k20 <teefisher314@gmail.com>
2021-03-23 11:37:40 -0500

    Test the equals Method testJobsForEquality created

--- src/Tests/JobTest.java
+++ src/Tests/JobTest.java
@@ -27,41 +27,21 @@ public class JobTest {
         assertTrue(job2.getId() - job1.getId() >= 1);
 
     }
+
+    @Test
+    public void testJobConstructorSetsAllFields() {
+        Job jobFullConstructor = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        assertTrue(jobFullConstructor instanceof Job);
+    }
+
+    @Test
+    public void testJobsForEquality() {
+        Job job1 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        Job job2 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        assertNotEquals(job1.getId(), job2.getId());
+    }
+
+
 }
-//    @Test
-//    public void testJobConstructorSetsAllFields() {
-//        Job job = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
-//        assertEquals("value", id, .001);
-//
-//    }
-//
-//    @Test
-//    public void testJobsForEquality() {
-//        Job job = new Job("Full-Stack Developer", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
-//        Job job1 = new Job("Full-Stack Developer", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
-//        assertFalse(job != job1);
-//    }
-//
-//    @Test
-////When passed a Job object, it should return a string that contains a blank line before and after the job information
-//    public void hasStringThatContainsBlankLineBeforeAndAfterJobInfo() {
-//        assertTrue(Job.hasStringThatContainsBlankLineBeforeAndAfterJobInfo(" "));
-//    }
-//
-//    @Test
-////The string should contain a label for each field, followed by the data stored in that field. Each field should be on its own line
-//    public void containsLabelForEachField() {
-//        assertTrue(Job.containsLabelForEachField(",", "\n"));
-//    }
-//
-//    @Test//If a field is empty, the method should add, “Data not available” after the label
-//    public void ifFieldIsEmpty() {
-//        assertTrue(Job.ifFieldIsEmpty(" ", true));
-//    }
-//
-//    @Test//(Bonus) If a Job object ONLY contains data for the id field, the method should return, “OOPS! This job does not seem to exist.”
-//    public void ifJobContainsData() {
-//        assertTrue(Job.ifJobContainsDataForIdField(id, true));
-//    }
-//    }
-//
+
+
